f2d1d2170de89ee428feeff6dd92dbfacb16f1f8
Chase A Smith <74937395+smit9804@users.noreply.github.com>
2022-03-18 13:33:06 -0500

    Update settings.py

--- aegSite/settings.py
+++ aegSite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p26#_e^pq3y9mcliq-^gz8o4^0y$q8muz!zot!-qx))lm9+8si'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.145.128.102']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
